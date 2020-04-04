64ea1ca5914932e5b7100bde8c22bc61eca1705d
sogatanco <adeliapucha46@gmail.com>
2020-04-04 12:58:54 +0700

    Initial commit

---
+++ app/src/main/java/site/team2dev/m_lerning/AdminProdi.java
@@ -0,0 +1,14 @@
+package site.team2dev.m_lerning;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminProdi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_prodi);
+    }
+}
---
+++ app/src/main/java/site/team2dev/m_lerning/LoginActivity.java
@@ -0,0 +1,14 @@
+package site.team2dev.m_lerning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
